8a8401741f69db14d68f25c70a667e52024843bb
Wenting Zhao <wentingzhao@outlook.com>
2022-03-27 22:52:45 -0400

    Added support for aNLI.

--- modeling/dataset.py
+++ modeling/dataset.py
@@ -7,90 +7,102 @@ from transformers import AutoTokenizer
 from datasets import load_dataset
 from generation_example import all_relations
 
-
-def preprocess_function(examples, tokenizer, ending_names, baseline=False):
-    num_answers = len(ending_names)
-    #num_triplets = len(examples[0]['cst'])
-    # Repeat each first sentence four times to go with the four possibilities of second sentences.
-    if baseline:
-        first_sentences = [[f"{e['context']} {tokenizer.sep_token} {e['question']}"] * num_answers for e in examples]
-    else:
-        first_sentences = [[f"{e['context']} {t} {tokenizer.sep_token} {e['question']}" for t in e['cst']] * num_answers for e in examples]
-    first_sentences = sum(first_sentences, [])
-
-    # Grab all second sentences possible for each context.
-    if baseline:
-        second_sentences = [[f"{e[end]}"] for e in examples for end in ending_names]
-    else:
-        second_sentences = [[f"{e[end]}"] * len(e['cst']) for e in examples for end in ending_names]
-    lens = [0]
-    for i, s in enumerate(second_sentences):
-        if i % 3 == 0:
-            last = lens[-1]
-            lens.append(len(s)*3+last)
-
-    second_sentences = sum(second_sentences, [])
-    assert len(first_sentences) == len(second_sentences)
-
-    print("start to tokenize")
-    # Tokenize
-    tokenized_examples = tokenizer(first_sentences, second_sentences, truncation=True)
-    # Un-flatten
-    print("done tokenize")
-    if baseline:
-        return {k: [v[i:i+num_answers] for i in range(0, len(v), num_answers)] for k, v in tokenized_examples.items()}
-    else:
-        return {k: [v[lens[i]:lens[i+1]] for i in range(len(lens)-1)] for k, v in tokenized_examples.items()}
-
-def prepare(filename, split, path, baseline=False):
-    print("preparing ", filename)
-    ending_names = ["answerA", "answerB", "answerC"]
-    data = []
-    with open(filename, 'r') as fin:
-        for line in fin:
-            data.append(json.loads(line))
-
-    raw0, raw1 = torch.load(f"cache/res_{split}.pt")
-    raw0 = [item for sub in raw0 for item in sub]
-    raw1 = [item for sub in raw1 for item in sub]
-    num_examples = len(data) 
-    csts = []
-    scores = []
-    interval = len(raw0)//num_examples
-    for i in range(0, len(raw0), interval):
-        csts.append([x for j, x in enumerate(raw0[i:i+interval])])
-        scores.append([x for j, x in enumerate(raw1[i:i+interval])])
-
-    tokenizer = AutoTokenizer.from_pretrained(path, use_fast=True)
-    assert len(data) == len(csts) == len(scores)
-    for example, cst, score in zip(data, csts, scores):
-        example['cst'] = [x+y for x, y in zip(all_relations, cst) if y.strip() != 'none']
-        example['score'] = [x for x, y in zip(score, cst) if y.strip() != 'none']
-        assert len(example['cst']) == len(example['score'])
-    if os.path.isfile(f"cache/{split}_encodings.pkl"):
-        with open(f"cache/{split}_encodings.pkl", 'rb') as f:
-            features = pickle.load(f)
-    else:
-        features = preprocess_function(data, tokenizer, ending_names, baseline)
-        with open(f"cache/{split}_encodings.pkl", 'wb') as f:
-            pickle.dump(features, f)
-    features['scores'] = [e['score'] * len(ending_names) for e in data]
-    features['cqs'] = [[f"{e['context']} {tokenizer.sep_token} {e['question']} {tokenizer.sep_token} {r}" for c, r in zip(cst, all_relations) if c.strip() != 'none'] for cst, e in zip(csts, data)]
-    features['cqs'] = [tokenizer(l, padding='longest', return_tensors='pt') for l in features['cqs']]
-    labels = []
-    for item in data:
-        label = item['correct']
-        if label == 'A':
-            labels.append(0)
-        elif label == 'B':
-            labels.append(1)
-        elif label == 'C':
-            labels.append(2)
-    return features, labels
-
 class Dataset(torch.utils.data.Dataset):
+    def preprocess_function(self, examples, tokenizer, ending_names, baseline=False):
+        num_answers = len(ending_names)
+        # Repeat each first sentence four times to go with the four possibilities of second sentences.
+        if baseline:
+            first_sentences = [[f"{e['context']} {tokenizer.sep_token} {e['question']}"] * num_answers for e in examples]
+        else:
+            first_sentences = [[f"{e['context']} {tokenizer.sep_token} {t} {tokenizer.sep_token} {e['question']}" for t in e['cst']] * num_answers for e in examples]
+        first_sentences = sum(first_sentences, [])
+
+        # Grab all second sentences possible for each context.
+        if baseline:
+            second_sentences = [[f"{e[end]}"] for e in examples for end in ending_names]
+        else:
+            second_sentences = [[f"{e[end]}"] * len(e['cst']) for e in examples for end in ending_names]
+        lens = [0]
+        for i, s in enumerate(second_sentences):
+            if i % num_answers == 0:
+                last = lens[-1]
+                lens.append(len(s)*num_answers+last)
+
+        second_sentences = sum(second_sentences, [])
+        assert len(first_sentences) == len(second_sentences)
+
+        print("start to tokenize")
+        # Tokenize
+        tokenized_examples = tokenizer(first_sentences, second_sentences, truncation=True)
+        # Un-flatten
+        print("done tokenize")
+        if baseline:
+            return {k: [v[i:i+num_answers] for i in range(0, len(v), num_answers)] for k, v in tokenized_examples.items()}
+        else:
+            return {k: [v[lens[i]:lens[i+1]] for i in range(len(lens)-1)] for k, v in tokenized_examples.items()}
+
+    def prepare(self, filename, split, path, baseline=False):
+        print("preparing ", filename)
+        #ending_names = ["answerA", "answerB", "answerC"]
+        ending_names = ["answer0", "answer1", "answer2", "answer3"]
+        data = []
+        with open(filename, 'r') as fin:
+            for line in fin:
+                data.append(json.loads(line))
+
+        raw0, raw1 = torch.load(f"cache/res_{split}.pt")
+        raw0 = [item for sub in raw0 for item in sub]
+        raw1 = [item for sub in raw1 for item in sub]
+        num_examples = len(data) 
+        csts = []
+        #scores = []
+        interval = len(raw0)//num_examples
+        for i in range(0, len(raw0), interval):
+            csts.append([x for j, x in enumerate(raw0[i:i+interval])])
+            #scores.append([x for j, x in enumerate(raw1[i:i+interval])])
+
+        tokenizer = AutoTokenizer.from_pretrained(path, use_fast=True)
+        #assert len(data) == len(csts) == len(scores)
+        assert len(data) == len(csts)
+        #for example, cst, score in zip(data, csts, scores):
+        for example, cst in zip(data, csts):
+            if_none = [(y.strip() in {'none', ''}) for y in cst]
+            example['cst'] = set([f"{self.sym2words[x]}{y}" for x, y, z in zip(all_relations, cst, if_none) if not z])
+            if len(example['cst']) == 0: 
+                example['cst'] = [tokenizer.sep_token]
+            #example['cst'] = [y for y in cst if y.strip() != 'none']
+            #example['score'] = [x for x, y, z in zip(score, cst, if_none) if not z]
+            #for h in range(len(example['score'])):
+            #    example['score'][h] = [hh for hh in example['score'][h] if hh != 1.0]
+            #assert len(example['cst']) == len(example['score'])
+        if os.path.isfile(f"cache/{split}_encodings.pkl"):
+            with open(f"cache/{split}_encodings.pkl", 'rb') as f:
+                features = pickle.load(f)
+        else:
+            features = self.preprocess_function(data, tokenizer, ending_names, baseline)
+            with open(f"cache/{split}_encodings.pkl", 'wb') as f:
+                pickle.dump(features, f)
+        #features['scores'] = [e['score'] * len(ending_names) for e in data]
+        #features['cqs'] = [[f"{e['context']} {tokenizer.sep_token} {e['question']} {tokenizer.sep_token} {r}" for c, r in zip(cst, all_relations) if c.strip() != 'none'] for cst, e in zip(csts, data)]
+        #features['cqs'] = [tokenizer(l, padding='longest', return_tensors='pt') for l in features['cqs']]
+        labels = []
+        for item in data:
+            # socialIQA
+            #label = item['correct']
+            #if label == 'A':
+            #    labels.append(0)
+            #elif label == 'B':
+            #    labels.append(1)
+            #elif label == 'C':
+            #    labels.append(2)
+            # COSMOSQA
+            labels.append(int(item['label']))
+        #torch.save(data, "test_data.pt")
+        return features, labels
+
     def __init__(self, filename, split, path, baseline=False):
-        self.encodings, self.labels = prepare(filename, split, path, baseline)
+        self.sym2words = {"xWant": "PersonX wants", "xReact": "PersonX is", "xNeed": "PersonX needs", "xIntent": "PersonX wants", "xAttr": "PersonX is", "xEffect": "PersonX", "oReact": "PersonX is", "oEffect": "PersonX", "oWant": "PersonX wants", "xReason": "because"}
+        self.encodings, self.labels = self.prepare(filename, split, path, baseline)
 
     def __getitem__(self, idx):
         item = {key: val[idx] for key, val in self.encodings.items()}
@@ -100,3 +112,49 @@ class Dataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+class AlphaNLIDataset(torch.utils.data.Dataset):
+    def preprocess_function(self, examples, tokenizer, ending_names):
+        premise = "obs1"
+        first_sentences = [[f"{example[premise]} {example[end]}" for end in ending_names] for example in examples]
+        second_sentences = [[example["obs2"]] * len(ending_names) for example in examples]
+
+        first_sentences = sum(first_sentences, [])
+        second_sentences = sum(second_sentences, [])
+
+        tokenized_examples = tokenizer(first_sentences, second_sentences, truncation=True)
+        return {k: [v[i:i+len(ending_names)] for i in range(0, len(v), len(ending_names))] for k, v in tokenized_examples.items()}
+
+    def prepare(self, filename, path):
+        print("preparing ", filename)
+        tokenizer = AutoTokenizer.from_pretrained(path, use_fast=True)
+        ending_names = ["hyp1", "hyp2"]
+        data = []
+        labels = []
+        with open(filename, 'r') as fin:
+            for line in fin:
+                data.append(json.loads(line))
+        with open(filename.replace(".jsonl", "-labels.lst"), 'r') as fin:
+            for idx, line in enumerate(fin):
+                labels.append(int(line) - 1)
+        if "train" in filename:
+            if os.path.isfile(f"cache/anli_encodings.pkl"):
+                with open(f"cache/anli_encodings.pkl", 'rb') as f:
+                    features = pickle.load(f)
+            else:
+                features = self.preprocess_function(data, tokenizer, ending_names)
+                with open(f"cache/anli_encodings.pkl", 'wb') as f:
+                    pickle.dump(features, f)
+        else:
+            features = self.preprocess_function(data, tokenizer, ending_names)
+        return features, labels
+
+    def __init__(self, filename, path):
+        self.encodings, self.labels = self.prepare(filename, path)
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels[idx]
+        return item
+
+    def __len__(self):
+        return len(self.labels)
--- modeling/train_cr.py
+++ modeling/train_cr.py
@@ -17,8 +17,8 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from dataset import Dataset
 from tqdm.auto import tqdm
-from torch.nn import CrossEntropyLoss
 from torch.nn import NLLLoss
+from torch.distributions import Categorical
 import wandb
 wandb.login()
 
@@ -32,10 +32,10 @@ class DataCollatorForMultipleChoice:
     def __call__(self, features):
         label_name = "label" if "label" in features[0].keys() else "labels"
         labels = [feature.pop(label_name) for feature in features]
-        cqs = [feature.pop("cqs") for feature in features]
-        scores = []
-        for feature in features:
-            scores += feature.pop("scores")
+        #cqs = [feature.pop("cqs") for feature in features]
+        #scores = []
+        #for feature in features:
+        #    scores += feature.pop("scores")
         batch_size = len(features)
         num_choices = len(features[0]["input_ids"])
         flattened_features = [
@@ -55,16 +55,17 @@ class DataCollatorForMultipleChoice:
         #batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
         # Add back labels
         batch["labels"] = torch.tensor(labels, dtype=torch.int64)
-        batch["cqs"] = cqs
-        batch["scores"] = torch.tensor([np.prod(l) for l in scores], dtype=torch.float)
-        batch["scores"] = batch["scores"].view(-1, 1)
+        #batch["cqs"] = cqs
+        #batch["scores"] = torch.tensor([np.prod(l) for l in scores], dtype=torch.float)
+        #batch["scores"] = torch.tensor([1/np.sum(l)/len(l) for l in scores], dtype=torch.float)
+        #batch["scores"] = batch["scores"].view(-1, 1)
         return batch
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_path", type=str, required=True)
     parser.add_argument("--valid_path", type=str, required=True)
-    parser.add_argument("--test_path", type=str, required=True)
+    parser.add_argument("--test_path", type=str)
     parser.add_argument('--baseline', action='store_true')
     parser.add_argument('--nolog', action='store_true')
     parser.add_argument("--minimum", default=0.62, type=float,
@@ -85,7 +86,7 @@ def get_args():
                         help="The directory where the pretrained model will be saved.")
     parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay to use.")
     parser.add_argument(
-        "--num_warmup_steps", type=int, default=0, help="Number of steps for the warmup in the lr scheduler."
+        "--warmup_ratio", type=float, default=0, help="Warmup ratio in the lr scheduler."
     )
     parser.add_argument(
         "--learning_rate",
@@ -112,19 +113,19 @@ def get_args():
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    set_seed(103)
+    set_seed(555)
     logger = logging.getLogger(__name__)
 
     args = get_args()
     train_dataset = Dataset(args.train_path, 'train', args.model_dir, args.baseline)
     eval_dataset = Dataset(args.valid_path, 'dev', args.model_dir, args.baseline)
-    test_dataset = Dataset(args.test_path, 'test', args.model_dir, args.baseline)
+    #test_dataset = Dataset(args.test_path, 'test', args.model_dir, args.baseline)
     tokenizer = AutoTokenizer.from_pretrained(args.model_dir)
 
     data_collator = DataCollatorForMultipleChoice(tokenizer, padding='longest')
     train_dataloader = DataLoader(train_dataset, shuffle=True, collate_fn=data_collator, batch_size=args.batch_size)
     eval_dataloader = DataLoader(eval_dataset, collate_fn=data_collator, batch_size=args.eval_batch_size)
-    test_dataloader = DataLoader(test_dataset, collate_fn=data_collator, batch_size=args.eval_batch_size)
+    #test_dataloader = DataLoader(test_dataset, collate_fn=data_collator, batch_size=args.eval_batch_size)
 
     def get_model_optim():
         model = AutoModel.from_pretrained(args.model_dir)
@@ -154,46 +155,55 @@ def main():
     lr_scheduler_a = get_scheduler(
         name=args.lr_scheduler_type,
         optimizer=optim_a,
-        num_warmup_steps=args.num_warmup_steps,
+        num_warmup_steps=int(args.warmup_ratio*args.max_train_steps),
         num_training_steps=args.max_train_steps,
     )
 
-    if not args.baseline:
-        model_r, optim_r = get_model_optim()
-        dropout_r = nn.Dropout(model_r.config.hidden_dropout_prob)
-        classifier_r = nn.Linear(model_r.config.hidden_size, 1)
-        classifier_r = classifier_r.to(device)
-        model_r.to(device)
-        lr_scheduler_r = get_scheduler(
-            name=args.lr_scheduler_type,
-            optimizer=optim_r,
-            num_warmup_steps=args.num_warmup_steps,
-            num_training_steps=args.max_train_steps,
-        )
+    #if not args.baseline:
+    #    model_r, optim_r = get_model_optim()
+    #    dropout_r = nn.Dropout(model_r.config.hidden_dropout_prob)
+    #    classifier_r = nn.Linear(model_r.config.hidden_size, 1)
+    #    classifier_r = classifier_r.to(device)
+    #    model_r.to(device)
+    #    lr_scheduler_r = get_scheduler(
+    #        name=args.lr_scheduler_type,
+    #        optimizer=optim_r,
+    #        num_warmup_steps=args.num_warmup_steps,
+    #        num_training_steps=args.max_train_steps,
+    #    )
+    #model_a = AutoModel.from_pretrained("saved_models_a").cuda()
+    #model_r = AutoModel.from_pretrained("saved_models_r").cuda()
+    #classifier_a = torch.load(f"saved_models_a/classifier.pt").cuda()
+    #classifier_r = torch.load(f"saved_models_r/classifier.pt").cuda()
 
     loss_fct = NLLLoss()
     m = nn.Softmax(dim=0)
+    m1 = nn.LogSoftmax(dim=-1)
 
-    def compute_logits(outputs, b, outputs_r=None):
+    def compute_logits(outputs, b=None, train=True, outputs_r=None):
         pooled_output = outputs[1]
-        pooled_output = dropout_a(pooled_output)
+        if train:
+            pooled_output = dropout_a(pooled_output)
         logits = classifier_a(pooled_output)
         if not args.baseline:
-            pooled_output_r = outputs_r[1]
-            pooled_output_r = dropout_r(pooled_output_r)
-            logits_r = classifier_r(pooled_output_r)
-            probs_r = m(logits_r)
+            #pooled_output_r = outputs_r[1]
+            #if train:
+            #    pooled_output_r = dropout_r(pooled_output_r)
+            #logits_r = classifier_r(pooled_output_r)
+            #probs_r = m(logits_r)
             probs = m(logits)
-            probs = probs.T * b['scores'].view(1, -1)
+            probs = probs.T #* b['scores'].view(1, -1)
             #reshaped_probs = probs.view(-1, args.num_choice, args.num_cst)
             reshaped_probs = probs.view(1, args.num_choice, -1)
-            reshaped_probs = torch.permute(reshaped_probs, (0, 2, 1))
-            reshaped_probs = reshaped_probs * probs_r
-            reshaped_probs = torch.permute(reshaped_probs, (0, 2, 1))
+            #print(reshaped_probs)
+            #reshaped_probs = torch.permute(reshaped_probs, (0, 2, 1))
+            #reshaped_probs = reshaped_probs * probs_r
+            #reshaped_probs = torch.permute(reshaped_probs, (0, 2, 1))
             reshaped_probs = torch.sum(reshaped_probs, dim=-1)
-            return reshaped_probs
+            #return reshaped_probs, probs_r
+            return torch.log(reshaped_probs)
         else:
-            return m(logits).view(-1, args.num_choice)
+            return m1(logits.view(-1, args.num_choice))
  
     metric = load_metric("accuracy")
 
@@ -209,41 +219,50 @@ def main():
 
     name = "baseline" if args.baseline else "generative"
     if not args.nolog:
-        wandb.init(name=f'{name} training (joint): model-{args.model_dir} lr-{args.learning_rate} b-{args.batch_size*args.gradient_accumulation_steps}',
-               project='generative cr',
-               tags=['SocialIQA'])
+        wandb.init(name=f'{name} training (joint): model-{args.model_dir} lr-{args.learning_rate} b-{args.batch_size*args.gradient_accumulation_steps} l2-{args.weight_decay}',
+               project='generative cr cosmosQA',
+               #tags=['SocialIQA'])
+               tags=['cosmosQA'])
         wandb.config.lr = args.learning_rate
         wandb.watch(model_a)
-        if not args.baseline:
-            wandb.watch(model_r)
+        #if not args.baseline:
+        #    wandb.watch(model_r)
     best_valid = 0
 
     def evaluate(dataloader, split):
         model_a.eval()
-        if not args.baseline:
-            model_r.eval()
+        #res = []
+        #if not args.baseline:
+        #    model_r.eval()
         for step, eval_batch in enumerate(dataloader):
-            eval_batch_r = deepcopy(eval_batch['cqs'][0])
-            del eval_batch['cqs']
-            for key in eval_batch_r:
-                eval_batch_r[key] = eval_batch_r[key].to(device)
+            #eval_batch_r = deepcopy(eval_batch['cqs'][0])
+            #del eval_batch['cqs']
+            #for key in eval_batch_r:
+            #    eval_batch_r[key] = eval_batch_r[key].to(device)
             for key in eval_batch:
                 eval_batch[key] = eval_batch[key].to(device)
             with torch.no_grad():
                 outputs = model_a(input_ids=eval_batch["input_ids"], attention_mask=eval_batch["attention_mask"])
-                if not args.baseline:
-                    outputs_r = model_r(input_ids=eval_batch_r["input_ids"], attention_mask=eval_batch_r["attention_mask"])
+                #if not args.baseline:
+                #    outputs_r = model_r(input_ids=eval_batch_r["input_ids"], attention_mask=eval_batch_r["attention_mask"])
             if not args.baseline:
-                probs = compute_logits(outputs, eval_batch, outputs_r)
+                #probs, r_probs = compute_logits(outputs, eval_batch, False, outputs_r)
+                probs = compute_logits(outputs, eval_batch, False)
             else:
-                probs = compute_logits(outputs)
+                probs = compute_logits(outputs, train=False)
             predictions = probs.argmax(dim=-1)
+            #pooled_output = outputs[1]
+            #logits = classifier_a(pooled_output)
+            #probs2 = m(logits)
+            #res.append(predictions)
+            #res.append(probs2)
             metric.add_batch(
                 predictions=predictions,
                 references=eval_batch["labels"],
             )
 
         eval_metric = metric.compute()
+        #torch.save(res, "res.pt")
         if not args.nolog:
             wandb.log({
                 "step": completed_steps,
@@ -252,33 +271,38 @@ def main():
 
     for epoch in range(args.epoch):
         model_a.train()
-        if not args.baseline:
-            model_r.train()
+        #if not args.baseline:
+        #    model_r.train()
         for step, batch in enumerate(train_dataloader):
-            batch_r = deepcopy(batch['cqs'][0])
-            del batch['cqs']
-            for key in batch_r:
-                batch_r[key] = batch_r[key].to(device)
+            #evaluate(test_dataloader, "Test")
+            #return
+            #batch_r = deepcopy(batch['cqs'][0])
+            #del batch['cqs']
+            #for key in batch_r:
+            #    batch_r[key] = batch_r[key].to(device)
             for key in batch:
                 batch[key] = batch[key].to(device)
             outputs = model_a(input_ids=batch["input_ids"], attention_mask=batch["attention_mask"])
             if not args.baseline:
-                outputs_r = model_r(input_ids=batch_r["input_ids"], attention_mask=batch_r["attention_mask"])
-            if not args.baseline:
-                probs = compute_logits(outputs, batch, outputs_r)
+                #outputs_r = model_r(input_ids=batch_r["input_ids"], attention_mask=batch_r["attention_mask"])
+                #probs, r_probs = compute_logits(outputs, batch, outputs_r=outputs_r)
+                probs = compute_logits(outputs, batch)
             else:
                 probs = compute_logits(outputs)
-            loss = loss_fct(torch.log(probs), batch['labels'])
-            loss = loss / args.gradient_accumulation_steps
+            loss = loss_fct(probs, batch['labels'])
+            #nll = loss_fct(torch.log(probs), batch['labels'])
+            #ent_loss = Categorical(raw).entropy().mean()
+            #loss = nll + ent_loss
+            #loss = loss / args.gradient_accumulation_steps
             loss.backward()
             if step % args.gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
                 optim_a.step()
                 lr_scheduler_a.step()
                 optim_a.zero_grad()
-                if not args.baseline:
-                    optim_r.step()
-                    lr_scheduler_r.step()
-                    optim_r.zero_grad()
+                #if not args.baseline:
+                #    optim_r.step()
+                #    lr_scheduler_r.step()
+                #    optim_r.zero_grad()
                 progress_bar.update(1)
                 completed_steps += 1
                 if not args.nolog:
@@ -288,12 +312,14 @@ def main():
 
             if step % (500*args.gradient_accumulation_steps) == 0:
                 acc = evaluate(eval_dataloader, "Valid")
-                if acc > best_valid and acc > args.minimum:
-                    evaluate(test_dataloader, "Test")
-                    best_valid = acc
-                    model_a.save_pretrained(f"{args.output_model_dir}_a")
-                    if not args.baseline:
-                        model_r.save_pretrained(f"{args.output_model_dir}_r")
+                #if acc > best_valid and acc > args.minimum:
+                #    evaluate(test_dataloader, "Test")
+                #    best_valid = acc
+                #    model_a.save_pretrained(f"{args.output_model_dir}_a")
+                #    torch.save(classifier_a, f"{args.output_model_dir}_a/classifier.pt")
+                    #if not args.baseline:
+                    #    model_r.save_pretrained(f"{args.output_model_dir}_r")
+                    #    torch.save(classifier_r, f"{args.output_model_dir}_r/classifier.pt")
 
 if __name__ == '__main__':
     main()
